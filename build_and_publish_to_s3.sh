4bfc2abbbb1b9f5e29455f7e5d966bff01b1008f
Humphrey Murray <hmurray@datalive.com.au>
2016-07-22 11:43:41 +1000

    Updating templates

--- build_and_publish_to_s3.sh
+++ build_and_publish_to_s3.sh
@@ -1,6 +1,6 @@
 
 # S3: http://humpty.xyz/
 # Run this from the root of the repository
-./build_resources.sh
+#./build_resources.sh
 hugo
 s3cmd sync -r --delete-removed public/ s3://hugo.songlib.com/
--- themes/songlib/build_resources.sh
+++ themes/songlib/build_resources.sh
@@ -1,4 +1,5 @@
 
-sassc --sourcemap resources/site.scss static/css/theme.css
+sassc --sourcemap --style=compressed resources/site.scss static/css/theme.css
 cp -R resources/bootstrap-sass-3.3.6/fonts/bootstrap static/fonts/
+cp -R resources/font-awesome-4.6.3/fonts/ static/fonts/font-awesome/
 cat resources/jquery-2.2.4.min.js resources/bootstrap-sass-3.3.6/javascripts/bootstrap.min.js > static/js/combined.min.js
